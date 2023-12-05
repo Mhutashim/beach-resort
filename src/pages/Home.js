f43d3b39f0308341d257257d71693d767e65ab5f
Mhutashim <billamdmhutashim@gmail.com>
2023-12-05 17:16:46 +0600

    Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner"; // it will be child of Hero
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       {/* The default prop is working here in Hero Component*/}
-      <Hero></Hero>
+      <Hero>
+        <Banner
+          title="Luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 export const Rooms = () => {
   return (
     <div>
       {/* here the roomsHero prop is overriding the default props */}
-      <Hero hero="roomsHero"></Hero>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
