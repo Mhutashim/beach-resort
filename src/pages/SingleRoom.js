896a466de9efb38839941c0700b6984da3cd7b5a
Mhutashim <billamdmhutashim@gmail.com>
2023-12-12 16:05:14 +0600

    single room done

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -61,15 +61,66 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
+    // console.log(images);
+
+    //array destructuring
+    //const [mainImg, img1, img2, img3] = images; //index-wise will be assigned
+
+    //array destructuring with spead operator (...) - to auto assign to other array
+    const [mainImg, ...remainingImg] = images; // remainingImg will be array
+    // console.log(remainingImg);
+
+    console.log(extras);
 
     return (
-      <StyledHero img={images[0] || defaultBcg}>
-        <Banner title={`${name} room `}>
-          <Link to={"/rooms"} className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg || defaultBcg}>
+          <Banner title={`${name} room `}>
+            <Link to={"/rooms"} className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {/* Will show all the images */}
+            {/* {images.map((image, index) => (
+              <img key={index} src={image} alt={name}></img>
+            ))} */}
+
+            {/* will show only the remaing imgs */}
+            {remainingImg.map((image, index) => (
+              <img key={index} src={image} alt={name}></img>
+            ))}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : {size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              {/* using the &&(AND) operator */}
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((extra, index) => (
+              <li key={index}>- {extra}</li>
+            ))}
+          </ul>
+        </section>
+      </>
     );
   }
 }
